c46b003cd5f6ad1d2002f6f1f03e7c6bdbeab429
David Mandelberg <david@mandelberg.org>
2017-12-08 16:33:56 -0500

    Fix instrument list bugs.
    1. Split lists of instruments into individual values.
    2. Remove prefixes that are not part of the instrument name.

--- personal-picard-plugins/instruments.py
+++ personal-picard-plugins/instruments.py
@@ -11,13 +11,55 @@ from picard.metadata import register_track_metadata_processor
 from picard.plugin import PluginPriority
 
 
+def iterate_instruments(instrument_list):
+    """Iterate over components of strings like "A, B and C"
+    """
+
+    remaining = instrument_list
+    while remaining:
+        instrument, sep, remaining = remaining.partition(', ')
+        if not remaining:
+            instrument, sep, remaining = instrument.partition(' and ')
+            if ' and ' in remaining:
+                raise ValueError(
+                    'Instrument list contains multiple \'and\'s: ' +
+                    instrument_list)
+
+            yield instrument
+
+def strip_instrument_prefixes(instrument):
+    instrument_prefixes = [
+        'additional ',
+        'guest ',
+        'solo ',
+        ]
+
+    remaining = instrument
+    while remaining:
+        removed_prefix = False
+        for prefix in instrument_prefixes:
+            if remaining.startswith(prefix):
+                remaining = remaining[len(prefix):]
+                removed_prefix = True
+        if not removed_prefix:
+            break
+    if not remaining:
+        raise ValueError(
+            'Instrument is empty after stripping prefixes: ' + instrument)
+
+    return remaining
+
 def add_instruments(tagger, metadata, *args):
     key_prefix = 'performer:'
+
     for key, values in metadata.rawitems():
         if not key.startswith(key_prefix):
             continue
-        instrument = key[len(key_prefix):]
-        metadata.add_unique('~instrument', instrument)
+
+        for instrument in iterate_instruments(key[len(key_prefix):]):
+            metadata.add_unique(
+                '~instrument',
+                strip_instrument_prefixes(instrument))
 
 register_track_metadata_processor(
     add_instruments,
